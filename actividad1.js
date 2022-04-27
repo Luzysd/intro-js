959e56a3b72389d8a6df4f6a8f194de50a07ed37
Luzysd <101684944+Luzysd@users.noreply.github.com>
2022-04-26 21:36:01 -0500

    Add files via upload

---
+++ actividad1.js
@@ -0,0 +1,44 @@
+// actividad 1.1//
+prompt("ingresa un numero")
+'15'
+var valor1= 15
+undefined
+var valor2= 2
+undefined
+var mult= valor1 * valor2
+undefined
+console.log (mult)
+VM849:1 30
+alert("El resultado es: " + 30)
+
+// actividad 1.2//
+prompt("ingresa un numero")
+'45'
+prompt( "ingresa un numero")
+'30'
+var sub= 45 - 30
+undefined
+console.log (sub)
+VM622:1 15
+
+// actividad 1.3//
+var radio=5.3
+undefined
+var pi= 3.1416
+undefined
+var area= pi*radio**2
+    
+    
+undefined
+console.log (area)
+VM1169:1 88.247544
+
+// actividad 1.4//
+var D=8
+undefined
+var d=6
+undefined
+var area= D*d/2
+undefined
+document.write (area= D*d/2)
+VM1323:1 24
